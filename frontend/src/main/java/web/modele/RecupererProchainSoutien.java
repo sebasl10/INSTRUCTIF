cb24be9f0bd4e5b73a7fbdf5d7893a5a0bdd149c
lletouze <lletouze@2D-3IF-DASI-02.vdi.insa-lyon.fr>
2025-05-30 16:18:10 +0200

    Prochaint Soutien  DashboardIntervenant

---
+++ frontend/src/main/java/web/modele/RecupererProchainSoutien.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.modele;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import metier.modele.Intervenant;
+import metier.modele.Personne;
+import metier.modele.Soutien;
+import metier.service.Service;
+
+/**
+ *
+ * @author lletouze
+ */
+public class RecupererProchainSoutien extends Action{
+
+    public RecupererProchainSoutien(Service service) {
+        super(service);
+    }
+
+    @Override
+    public void execute(HttpServletRequest request) {
+        Intervenant monIntervenant = null;
+        Soutien monSoutien = null;
+        try {
+            HttpSession session = request.getSession();
+            Personne maPersonne = service.rechercherPersonneParId((Long) session.getAttribute("id"));
+            monIntervenant = service.rechercherIntervenant(maPersonne.getMail());
+            monSoutien = service.recupererSoutienEnAttente(monIntervenant);
+        } catch (Exception ex) {
+            Logger.getLogger(listerMatiere.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        request.setAttribute("monIntervenant", monIntervenant);
+        request.setAttribute("historique", monSoutien);
+    }
+
+}
