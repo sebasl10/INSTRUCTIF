496f3e27fed1745bf1f48c0e38914275a3b97671
lletouze <lletouze@2D-3IF-DASI-10.vdi.insa-lyon.fr>
2025-06-01 12:18:35 +0200

    Empecher demander 2 soutiens

--- frontend/src/main/java/web/vue/listerMatiereSerialisation.java
+++ frontend/src/main/java/web/vue/listerMatiereSerialisation.java
@@ -6,8 +6,9 @@
 package web.vue;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,13 +23,22 @@ public class listerMatiereSerialisation extends Serialisation {
     @Override
     public void appliquer(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
-        List<Matiere> maListe = (List<Matiere>) request.getAttribute("listeMatiere");
+
+        List<Matiere> listeMatiere = (List<Matiere>) request.getAttribute("listeMatiere");
+        Boolean dejaSoutien = (Boolean) request.getAttribute("dejaSoutien");
+        if (dejaSoutien == null) {
+            dejaSoutien = false; // valeur par défaut si pas défini
+        }
+
         Gson gson = new Gson();
-        String json=gson.toJson(maListe);
-        System.out.println(json);
-        PrintWriter out = response.getWriter();
-        out.write(json);
-        out.close();
+
+        JsonObject finalJson = new JsonObject();
+        JsonElement matiereJson = gson.toJsonTree(listeMatiere);
+
+        finalJson.add("listeMatiere", matiereJson);
+        finalJson.addProperty("dejaSoutien", dejaSoutien);
+
+        response.getWriter().write(gson.toJson(finalJson));
     }
-    
+
 }
