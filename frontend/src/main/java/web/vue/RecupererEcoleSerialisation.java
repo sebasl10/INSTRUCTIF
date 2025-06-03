3e673255072637f6e5122e77c7e9525d867887c5
lletouze <lletouze@2D-3IF-DASI-12.vdi.insa-lyon.fr>
2025-06-03 10:08:11 +0200

    Suite

---
+++ frontend/src/main/java/web/vue/RecupererEcoleSerialisation.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web.vue;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import metier.modele.Ecole;
+import metier.modele.Matiere;
+
+/**
+ *
+ * @author lletouze
+ */
+public class RecupererEcoleSerialisation extends Serialisation {
+
+    @Override
+    public void appliquer(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        List<Ecole> ecoles = (List<Ecole>) request.getAttribute("ecoles");
+        Gson gson = new Gson();
+        JsonObject finalJson = new JsonObject();
+        JsonElement ecolesJson = gson.toJsonTree(ecoles);
+        finalJson.add("ecoles", ecolesJson);
+        response.getWriter().write(gson.toJson(finalJson));
+    }
+
+}
