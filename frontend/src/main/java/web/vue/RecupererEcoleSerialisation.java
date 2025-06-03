8112db757571d4f47579346877c996b29ee25ae8
lletouze <lletouze@2D-3IF-DASI-12.vdi.insa-lyon.fr>
2025-06-03 11:16:22 +0200

    CARTE FINI

--- frontend/src/main/java/web/vue/RecupererEcoleSerialisation.java
+++ frontend/src/main/java/web/vue/RecupererEcoleSerialisation.java
@@ -9,11 +9,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.IOException;
+import static java.lang.System.out;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import metier.modele.Ecole;
-import metier.modele.Matiere;
 
 /**
  *
@@ -23,13 +23,15 @@ public class RecupererEcoleSerialisation extends Serialisation {
 
     @Override
     public void appliquer(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
-        List<Ecole> ecoles = (List<Ecole>) request.getAttribute("ecoles");
-        Gson gson = new Gson();
-        JsonObject finalJson = new JsonObject();
-        JsonElement ecolesJson = gson.toJsonTree(ecoles);
-        finalJson.add("ecoles", ecolesJson);
-        response.getWriter().write(gson.toJson(finalJson));
-    }
+    response.setContentType("application/json");
+    List<Ecole> ecoles = (List<Ecole>) request.getAttribute("ecoles");
+    Gson gson = new Gson();
+    JsonObject finalJson = new JsonObject();
+    JsonElement ecolesJson = gson.toJsonTree(ecoles);
+    finalJson.add("ecoles", ecolesJson);
+    System.out.println(finalJson);
+    response.getWriter().write(gson.toJson(finalJson));
+}
+
 
 }
